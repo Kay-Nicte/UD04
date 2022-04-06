6cae2955de1f4d240e20f1dd6bb7d869a670ce86
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-06 12:44:26 +0200

    "Ejercicio 4"

---
+++ UD04/src/UD04/Ejercicio4.java
@@ -0,0 +1,33 @@
+package UD04;
+
+public class Ejercicio4 {
+
+	public Ejercicio4() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Creo la variable n y le asigno un valor
+		int n = 9;
+		//Muestro por pantalla su valor inicial 
+		System.out.println("El valor inicial de n es "+n+".");
+		//Incremento n en 77
+		n = n+77;
+		//Muestro por pantalla el valor actual 
+		System.out.println("Incremento el valor de n en 77. Ahora n vale "+n+".");
+		//Decremento n en 3
+		n = n-3;
+		//Muestro por pantalla el valor actual 
+		System.out.println("Decremento el valor de n en 3. Ahora n vale "+n+".");
+		//Duplico el valor de n
+		n = n*2;
+		//Muestro por pantalla el valor actual 
+		System.out.println("Duplico el valor de n. Ahora n vale "+n+".");
+
+		
+		
+	}
+
+}
